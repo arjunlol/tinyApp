c1055c1e4c3864a894b13d3b54f8550bbeed5ec4
Arjun Lall <lallarjun@gmail.com>
2017-05-10 15:49:51 +0000

    created initial EJS template and passed url object

---
+++ express_server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+//use EJS as templating engine
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+//new route handler for /urls to pass URL data to template
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
